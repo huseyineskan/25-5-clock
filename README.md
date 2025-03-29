f40bf1ca5520182c3f16ed509bae6ef0eb18aaa6
Huseyin <huseyineskan01@gmail.com>
2025-03-29 01:32:52 +0100

    added all features and it works very well.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# 25+5 Clock
+# 25+5 Clock - with React
+
+Pomodoro, however, allows you to set the focus time and breaks yourself.
+
+## Features
+
+- Focus time can be adjusted.
+- Break time can be adjusted.
+- Pause can be done with Start/Pause.
+- Application can be reset with Reset.
+- It gives a warning when there is 1 minute left and the background color turns red.
+- When the break time starts, it gives a warning sound and the background color turns green.
+
+## Screenshot
+
+![25-5-Clock](https://github.com/huseyineskan/25-5-clock/blob/main/src/assets/img/clock.png)
--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -5,6 +5,15 @@ function Clock() {
   const [sessionCounter, setSessionCounter] = useState(25);
   const [second, setSecond] = useState(0);
   const [minute, setMinute] = useState(0);
+  const [startTimer, setStartTimer] = useState(false);
+  const [lastOneMinute, setLastOneMinute] = useState(false);
+  const [breakOn, setBreakOn] = useState(false);
+  const [bgColor, setBgColor] = useState("transparent");
+
+  function SetMusic(link) {
+    const audio = new Audio(link);
+    audio.play();
+  }
 
   function HandleBreakClick(value) {
     value > 0
@@ -16,17 +25,68 @@ function Clock() {
     value > 0
       ? setSessionCounter(Number(sessionCounter) + 1)
       : setSessionCounter(Number(sessionCounter) - 1);
+    setSecond(0);
+    setBgColor("black");
   }
 
-  function Timer() {
-    setSecond(Number(second) + 1);
+  function Session() {
+    if (second > 0) {
+      setSecond(second - 1);
+    } else {
+      setSecond(59);
+      setMinute(minute - 1);
+    }
+
+    if (minute == 0 && second == 0) {
+      setBreakOn((prev) => !prev);
+      breakOn ? setMinute(sessionCounter - 1) : setMinute(breakCounter - 1);
+    }
+
+    SetStyles();
   }
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     Timer();
-  //   }, 1000);
-  // });
+  function SetStyles() {
+    if (breakOn) {
+      setBgColor("#20bf6b");
+      second == 59 &&
+        SetMusic(
+          "https://cdn.freesound.org/previews/795/795303_17090683-lq.mp3"
+        );
+    } else {
+      setBgColor("#b71540");
+    }
+
+    if (minute == 0) {
+      setLastOneMinute(true);
+      second == 59 &&
+        !breakOn &&
+        SetMusic(
+          "https://cdn.freesound.org/previews/795/795492_5287430-lq.mp3"
+        );
+    } else {
+      setLastOneMinute(false);
+    }
+  }
+
+  function Reset() {
+    setStartTimer(false);
+    setBreakCounter(5);
+    setSessionCounter(25);
+    setMinute(sessionCounter);
+    setSecond(0);
+    setBreakOn(false);
+    setBgColor("black");
+  }
+
+  setTimeout(() => {
+    if (startTimer) {
+      Session();
+    }
+  }, 1000);
+
+  useEffect(() => {
+    setMinute(sessionCounter);
+  }, [sessionCounter]);
 
   return (
     <div className="clock-div">
@@ -68,15 +128,32 @@ function Clock() {
           </div>
         </div>
       </div>
-      <div className="clock-timer">
-        <p id="timer-label">Session</p>
+      <div
+        className="clock-timer"
+        style={
+          lastOneMinute || breakOn
+            ? { backgroundColor: bgColor, transition: "400ms ease" }
+            : { backgroundColor: "black", transition: "400ms ease" }
+        }
+      >
+        <p id="timer-label">{breakOn ? "Break" : "Session"}</p>
         <span id="time-left">
-          {minute} : {second}
+          {minute < 10 ? "0" + minute : minute} :{" "}
+          {second < 10 ? "0" + second : second}
         </span>
       </div>
       <div className="clock-controls">
-        <button id="start_stop">Start / Pause</button>
-        <button id="reset">Reset</button>
+        <button
+          id="start_stop"
+          onClick={() => {
+            startTimer ? setStartTimer(false) : setStartTimer(true);
+          }}
+        >
+          Start / Pause
+        </button>
+        <button id="reset" onClick={Reset}>
+          Reset
+        </button>
       </div>
     </div>
   );
